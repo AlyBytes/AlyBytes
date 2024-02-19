b725f907f5d8a0e3ee43df42046b0816575c7101
AlyCoding <aly.khamidullina@gmail.com>
2024-02-19 13:23:29 -0800

    this is practice

--- Sandbox/practice.py
+++ Sandbox/practice.py
@@ -1,14 +1,46 @@
-# practice by AlyBytes
+# def add(x,y):
+#     print("It will add numbers %d and %d" %(x,y))
+#     return x+y
+#
+# sum = add(2,3)
+# print("=", sum)
 
-def add(x, y):
-    print("It will add numbers %d and %d" % (x, y))
-    return x + y
+
+def compareTriplets(a, b):
+    count_a = 0
+    count_b = 0
+    new_arr = []
+
+    for i in range(3):
+        numAlice = int(a[i])
+
+        print(i)
+        print(numAlice)
+        for n in range(3):
+
+            numBob = int(b[n])
+            print(n)
+
+            print(numBob)
+            if numAlice > numBob:
+                count_a += 1
+
+            elif numAlice < numBob:
+                count_b += 1
+
+            else:
+                n=i
+                continue
+
+    new_arr.append(count_a)
+    new_arr.append(count_b)
+    print(new_arr)
 
 
 def main():
-    sum = add(2, 3)
-    print("=", sum)
+    a = [5, 6, 7]
+    b = [3, 6, 10]
+    compareTriplets(a, b)
 
 
 main()
-
